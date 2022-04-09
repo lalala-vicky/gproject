a0622dcdcd778ffb5878677868287c6f28d88eeb
qiong.zhou <qiong.zhou@lianwei.com.cn>
2022-04-09 23:07:31 +0800

    feat:project

--- src/main.js
+++ src/main.js
@@ -5,6 +5,31 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+// // 引入antd
+// import { createApp } from "vue";
+import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.css";
+// const app = createApp();
+// app.config.productionTip = false;
+Vue.use(Antd);
+// import { createApp } from "vue";
+// import Antd from "ant-design-vue";
+// // import App from "./App";
+// import "ant-design-vue/dist/antd.css";
+
+// const app = createApp();
+// app.config.productionTip = false;
+
+// app.use(Antd);
+
+// 引入echarts
+import echarts from "echarts";
+Vue.prototype.$echarts = echarts;
+
+// 引入axios
+import axios from "axios";
+Vue.prototype.$axios = axios;
+
 new Vue({
   router,
   store,
